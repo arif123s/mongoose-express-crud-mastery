abda5626ed22eb10bffb750289cbee06f37153b7
arif123s <arif.chuty@gmail.com>
2023-12-02 18:26:57 +0600

    user insert

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { UserRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 // const port = 3000
 
 //parsers
 app.use(express.json());
 app.use(cors());
+
+//application routes
+app.use('api/v1/users', UserRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
