f54544ecc0d2a86109edf005a3b2ad7eb7151880
arif123s <arif.chuty@gmail.com>
2023-12-04 18:09:20 +0600

    get all users

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -16,6 +16,22 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUsers = async (req:Request,res:Response)=>{
+try{
+const result = await UserServices.getAllUsersfromDB()
+
+ res.status(200).json({
+   success: true,
+   message: 'Users are retrieved successfully',
+   data: result,
+ });
+}
+catch(err){
+  console.log(err)
+}
+}
+
 export const UserControllers = {
   createUser,
+  getAllUsers
 };
